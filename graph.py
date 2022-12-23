d8d3512a6ab2daeb843bbce7f8f69e8992b7f060
Ander Stac <37026127+ahd3r@users.noreply.github.com>
2022-12-23 23:00:08 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -19,8 +19,27 @@ graph = [
     [0, 1]   # 5 vertex => 0, 5 => 1
 ]
 
-
 # Graph without direction and with weight
+graph = [
+    [[1, 3], [3, 5], [2, 2]],    # 0 vertex =(cost 3)=> 1, 0 =(cost 5)=> 3, 0 =(cost 2)=> 2
+    [[0, 3], [3, 1]],            # 1 vertex =(cost 3)=> 0, 1 =(cost 1)=> 3
+    [[0, 2], [4, 6], [5, 1]],    # 2 vertex =(cost 2)=> 0, 2 =(cost 6)=> 4, 2 =(cost 1)=> 5
+    [[0, 5], [1, 1], [4, 4]],    # 3 vertex =(cost 5)=> 0, 3 =(cost 1)=> 1, 3 =(cost 4)=> 4
+    [[2, 6], [3, 4], [5, 6]],    # 4 vertex =(cost 6)=> 2, 4 =(cost 4)=> 3, 4 =(cost 6)=> 5
+    [[2, 1], [4, 6]]             # 5 vertex =(cost 1)=> 2, 5 =(cost 6)=> 4
+]
+
+# Graph with direction and with weight
+graph = [
+    [[1, 3], [3, 5]],            # 0 vertex =(cost 3)=> 1, 0 =(cost 5)=> 3
+    [[0, 3]],                    # 1 vertex =(cost 3)=> 0
+    [[0, 2], [5, 1]],            # 2 vertex =(cost 2)=> 0, 2 =(cost 1)=> 5
+    [[0, 5], [1, 1], [4, 4]],    # 3 vertex =(cost 5)=> 0, 3 =(cost 1)=> 1, 3 =(cost 4)=> 4
+    [[2, 6], [5, 6]],            # 4 vertex =(cost 6)=> 2, 4 =(cost 6)=> 5
+    [[2, 1]]                     # 5 vertex =(cost 1)=> 2
+]
+
+# Advanced Graph without direction and with weight
 class Vertex:
     def __init__(self, id):
         self.id = id
@@ -71,7 +90,7 @@ class Graph:
         second_vert.adjectives[first_vert] = cost
 
 
-# Graph with direction and with weight
+# Advanced Graph with direction and with weight
 class Vertex:
     def __init__(self, id):
         self.id = id
